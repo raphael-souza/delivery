6cc971e1d7d304b898f63405e38d4760864f248d
raphael-souza <raphael3130@gmail.com>
2021-10-11 20:57:16 -0300

    criando usuário

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,10 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   before_action :set_order, only: [:show, :update, :destroy]
 
   # GET /orders
-  def index
+  def index 
     @orders = Order.all
 
     render json: @orders
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   resources :addresses
   resources :cities
   resources :states
-  devise_for :users
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
